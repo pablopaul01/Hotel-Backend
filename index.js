f77a9407954eac18997dc725078b17f07f85dd0c
pablopaul01 <juanpablosalomon@gmail.com>
2023-09-24 19:56:22 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const router = require("./routes")
+const dbConnection = require("./database/db");
+const cloudinary = require("cloudinary").v2;
+const jwtStrategy = require ("./passport/jwt")
+const passport = require ("passport")
+
+const app = express();
+
+//midleware
+app.use(cors());
+app.options('*', cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+
+app.listen(8080, ()=>{
+    console.log(`Servidor funcionando en el puerto ${8080}`)
+})
