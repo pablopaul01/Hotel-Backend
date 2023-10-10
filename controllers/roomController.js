017181a94cd4b86dad6b119a720e68316e0579e5
pablopaul01 <juanpablosalomon@gmail.com>
2023-10-10 01:47:37 -0300

    se agregan controladores y rutas de categorias de habitaciones

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -157,73 +157,7 @@ const updateCategorie = async (req, res) => {
     }
 }
 
-
-const updateRoom = async (req, res, next) => {
-    try {
-      const updatedRoom = await Room.findByIdAndUpdate(
-        req.params.id,
-        { $set: req.body },
-        { new: true }
-      );
-      res.status(200).json(updatedRoom);
-    } catch (err) {
-      next(err);
-    }
-  };
-const updateRoomAvailability = async (req, res, next) => {
-    try {
-      await Room.updateOne(
-        { "roomNumbers._id": req.params.id },
-        {
-          $push: {
-            "roomNumbers.$.unavailableDates": req.body.dates
-          },
-        }
-      );
-      res.status(200).json("Room status has been updated.");
-    } catch (err) {
-      next(err);
-    }
-  };
-const deleteRoom = async (req, res, next) => {
-    const hotelId = req.params.hotelid;
-    try {
-      await Room.findByIdAndDelete(req.params.id);
-      try {
-        await Hotel.findByIdAndUpdate(hotelId, {
-          $pull: { rooms: req.params.id },
-        });
-      } catch (err) {
-        next(err);
-      }
-      res.status(200).json("Room has been deleted.");
-    } catch (err) {
-      next(err);
-    }
-  };
-const getRoom = async (req, res, next) => {
-    try {
-      const room = await Room.findById(req.params.id);
-      res.status(200).json(room);
-    } catch (err) {
-      next(err);
-    }
-  };
-const getRooms = async (req, res, next) => {
-    try {
-      const rooms = await Room.find();
-      res.status(200).json(rooms);
-    } catch (err) {
-      next(err);
-    }
-  };
-
   module.exports = {
-    // getRooms,
-    // getRoom,
-    // deleteRoom,
-    // updateRoomAvailability,
-    // updateRoom,
     createRoom,
     getCategories,
     deleteCategorie,
