cca5ae10a05a0abc0f86bdbaf7564123be710f1c
unknown <eliana_f90@hotmail.com>
2023-10-28 21:14:21 -0300

    se eliminan comentarios routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const sendForm = require("../controllers/sendFormController");
 const authenticateAdmin = require("../middlewares/authAdmin");
 const authenticateUser = require("../middlewares/authUser");
 
-//rutas de usuarios
+
 router.get("/usuarios", authenticateAdmin, getAllUsers);
 router.get("/usuario/:id", authenticateAdmin, getUserById);
 router.delete("/usuario/:id", authenticateAdmin, deleteUser);
@@ -17,17 +17,17 @@ router.put("/desactivar/usuario/:id", authenticateAdmin, userDisabled);
 
 const upload = require("../middlewares/multer");
 
-//rutas de rooms
+
 router.post("/crear/categoria", authenticateAdmin, upload.array("imagenes", 5), createRoom);
 router.get("/categorias", getCategories);
 router.delete("/categoria/:id", authenticateAdmin, deleteCategorie);
-router.get("/categoria/:id", getCategorieById); //publica
+router.get("/categoria/:id", getCategorieById); 
 router.put("/categoria/:id", authenticateAdmin, updateCategorie);
 router.delete("/categoria/:id/room/:roomId", authenticateAdmin, deleteRoomFromCategory);
 router.put("/categoria/rooms/:id", authenticateAdmin, addRoomNumber);
 router.put("/categoria/:id/room/:roomId", authenticateUser, reserveRoomFromCategory);
 
-//ruta de formulario
+
 router.post("/formulario", sendForm)
 
 
